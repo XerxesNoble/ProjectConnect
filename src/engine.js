2189b2ef6c673ec346495df0b6de75b4e3acbb2a
Xerxes Noble <xerxesnoble@gmail.com>
2018-09-03 14:13:52 -0400

    feat(packaging): transpile/zip game and print size

--- src/engine.js
+++ src/engine.js
@@ -8,6 +8,7 @@ export default class Engine {
   constructor(context, hud) {
     this.hud = hud
     this.lives = 3 // Game constant?
+    
     this.views = {
       battery: document.getElementById('battery'),
       currentLevel: document.getElementById('currentLevel'),
